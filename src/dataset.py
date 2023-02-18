fix y calculation

src/dataset.py
@@ -16,7 +16,10 @@ CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 ## Create a custom Dataset class
 class CelebADataset(Dataset):
     def __init__(
-        self, root_dir=os.path.join(CUR_DIR, "../../data/celeba"), transform=None
+        self,
+        root_dir=os.path.join(CUR_DIR, "../../data/celeba"),
+        transform=None,
+        domains=["Male"],
     ):
         """
         Args:
@@ -52,17 +55,41 @@ class CelebADataset(Dataset):
         with open(f"{root_dir}/list_attr_celeba.txt") as f:
             for i, line in enumerate(f.readlines()):
                 line = re.sub(" *\n", "", line)
-                if i < 2:
+                if i == 0:
                     # first line - len of dataset, second - attributes header
                     continue
+                if i == 1:
+                    domain_indices = []
+                    header = re.split(" +", line)
+                    for domain in domains:
+                        domain_indices.append(header.index(domain))
+
                 else:
                     values = re.split(" +", line)
                     filename = values[0]
                     self.filenames.append(filename)
                     # delete contraint on num of annotations
-                    self.annotations.append([int(v) for v in values[1:]])
+                    annotations = []
+                    for idx in domain_indices:
+                        annotations.append(int(values[idx]))
+                    annotations = self._transform_targets(tuple(annotations), domains)
+                    self.annotations.append(annotations)
 
         self.annotations = np.array(self.annotations)
+
+    def _transform_targets(self, seq, domains):
+        from itertools import product
+
+        """Transforms sequence of form (-1,1,-1) to the form of (1,0,0,1,1,0)"""
+        new_seq = []
+        combs = list(product([-1, 1], repeat=len(domains)))
+        labels = {d: i for i, d in enumerate(combs)}
+        return labels[seq]
+
+        # for elem in seq:
+
+        #     new_seq.extend([0, 1]) if elem == 1 else new_seq.extend([1, 0])
+        return new_seq
 
     def __len__(self):
         return len(self.filenames)

src/model.py
@@ -104,8 +104,9 @@ class Discriminator(nn.Module):
         x = self.downsampling(x)
         x = self.conv(x)
         x = x.squeeze(3).squeeze(2)
-        output = self.out[y](x)
-        return output
+        out = torch.stack([layer(x) for layer in self.out], dim=1)
+        out = out[range(x.size(0)), y]
+        return out
 
 
 class MappingNetwork(nn.Module):
@@ -138,8 +139,9 @@ class MappingNetwork(nn.Module):
     def forward(self, z, y):
         z = self.backbone(z)
         # select y's head corresponding to
-        output = self.heads[y](z)
-        return output
+        out = torch.stack([layer(z) for layer in self.heads], dim=1)
+        out = out[range(z.size(0)), y, :]
+        return out
 
     def get_fc_block(self, in_ch, out_ch, activation=True):
         return (
@@ -183,8 +185,9 @@ class StyleEncoder(nn.Module):
         x = self.downsampling(x)
         x = self.conv(x)
         x = x.squeeze(3).squeeze(2)
-        output = self.out[y](x)
-        return output
+        out = torch.stack([layer(x) for layer in self.out], dim=1)
+        out = out[range(x.size(0)), y, :]
+        return out
 
 
 class ResBlock(nn.Module):

src/trainer.py
@@ -12,14 +12,11 @@ class Trainer:
         self.cfg = config
         self.device = self.cfg["training"]["device"]
         self.model = dict()
+        self.K = 2 ** len(self.cfg["data"]["domains"])
         self.model["gen"] = Generator()
-        self.model["disc"] = Discriminator(K=self.cfg["model"]["K"])
-        self.model["map"] = MappingNetwork(
-            K=self.cfg["model"]["K"], D=self.cfg["model"]["D"]
-        )
-        self.model["se"] = StyleEncoder(
-            K=self.cfg["model"]["K"], D=self.cfg["model"]["D"]
-        )
+        self.model["disc"] = Discriminator(K=self.K)
+        self.model["map"] = MappingNetwork(K=self.K, D=self.cfg["model"]["D"])
+        self.model["se"] = StyleEncoder(K=self.K, D=self.cfg["model"]["D"])
 
         # self.BS = BS
         # self.K = K
@@ -55,6 +52,7 @@ class Trainer:
             for batch in tqdm(self.dataloader):
                 # batch.to(self.device)
                 real = batch[0].to(self.device)
+                y_src = batch[1]["attributes"]
 
                 step += 1
 
@@ -76,8 +74,8 @@ class Trainer:
                     self.device
                 )
                 y_trg = torch.randint(
-                    size=(1, 1), low=0, high=self.cfg["model"]["K"] - 1
-                ).item()
+                    size=(self.cfg["training"]["batch_size"], 1), low=0, high=self.K - 1
+                ).squeeze()
                 s = self.model["map"](z, y_trg)  # shape of (B, num_domains, 16)
                 s2 = self.model["map"](z2, y_trg)  # shape of (B, num_domains, 16)
 
@@ -85,9 +83,9 @@ class Trainer:
                 fake2 = self.model["gen"](real, s2)
                 s_fake = self.model["se"](fake, y_trg)
 
-                fake_reversed = self.model["gen"](fake, self.model["se"](real, y_trg))
+                fake_reversed = self.model["gen"](fake, self.model["se"](real, y_src))
 
-                d_real = self.model["disc"](real, y_trg)
+                d_real = self.model["disc"](real, y_src)
                 d_fake_d = self.model["disc"](fake.detach(), y_trg)
                 d_fake_g = self.model["disc"](fake, y_trg)
 
@@ -115,7 +113,7 @@ class Trainer:
                 if step > 20:
                     break
 
-                if (self.log) and (step % self.config["log_steps"] == 0):
+                if (self.log) and (step % self.cfg["training"]["log_steps"] == 0):
                     gnorm_g = self.get_grad_norm(self.model["gen"])
                     gnorm_d = self.get_grad_norm(self.model["disc"])
                     gnorm_m = self.get_grad_norm(self.model["map"])
@@ -132,6 +130,7 @@ class Trainer:
                         style_rec_l.item(),
                         gnorms,
                     )
+                    self.log_images(fake)
 
     def log_scalars(
         self, step, epoch, loss_g, loss_d, cycle_l, style_div_l, style_rec_l, gnorms
@@ -146,10 +145,8 @@ class Trainer:
         for grad_norm, label in zip(gnorms, ["G_gn", "D_gn", "M_gn", "SE_gn"]):
             self.logger.add_scalar(label, grad_norm)
 
-    def log_images(
-        self,
-    ):
-        raise NotImplementedError
+    def log_images(self, fake):
+        self.logger.add_image("fake", fake)
 
     @torch.no_grad()
     def get_grad_norm(self, model, norm_type=2):

train.py
@@ -22,7 +22,10 @@ transform = transforms.Compose(
     ]
 )
 
-dataset = CelebADataset(root_dir="./data/", transform=transform)
+
+dataset = CelebADataset(
+    root_dir="./data/", transform=transform, domains=config["data"]["domains"]
+)
 num_workers = 0 if device == "cuda" else 2
 pin_memory = True if device == "cuda" else False
 dataloader = torch.utils.data.DataLoader(
@@ -32,30 +35,9 @@ dataloader = torch.utils.data.DataLoader(
     pin_memory=pin_memory,
     shuffle=True,
 )
-# generator = Generator()
-# discriminator = Discriminator(K=K)
-# mapping_network = MappingNetwork(K=K, D=D)
-# style_encoder = StyleEncoder(K=K, D=D)
 
 trainer = Trainer(config=config, dataloader=dataloader, log=False)
 
 
 if __name__ == "__main__":
     trainer.train()
-    # for i in range(3):
-    #     batch = next(iter(dataloader))
-    #     y_trg = torch.randint(size=(1, 1), low=0, high=K - 1).item()
-    #     im = batch[0]
-    #     z = torch.randn((BS, 16))
-    #     s = mapping_network(z, y_trg)
-    #     out = generator(im, s)
-    #     s_fake = style_encoder(out, y_trg)
-    #     out_reversed = generator(im, s_fake)
-    #     dout = discriminator(out_reversed, y_trg)
-    #     l1 = adversarial_loss(im, out)  # fix
-    #     l2 = style_rec_loss(s, s_fake)
-    #     l3 = style_div_loss(out_reversed, out_reversed)
-    #     l4 = cycle_loss(out_reversed, im)
-    #     l1.backward()
-    #     # print(l1, l2, l3, l4)
-    # gc.collect()