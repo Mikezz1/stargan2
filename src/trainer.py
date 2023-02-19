33bf9c0d1098e159a70aac477616d4f465af00a2
Михаил Олейник <mikhailoleynik@Mihails-MacBook-Pro.local>
2023-02-20 00:30:23 +0300

    add ema weights

--- src/trainer.py
+++ src/trainer.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 from src.model import *
 from torch.optim import AdamW
 from src.logger import WanDBWriter
+import copy
 
 
 class Trainer:
@@ -12,12 +13,22 @@ class Trainer:
         self.cfg = config
         self.device = self.cfg["training"]["device"]
         self.model = dict()
+        self.ema_model = dict()
         self.K = 2 ** len(self.cfg["data"]["domains"])
         size = self.cfg["data"]["size"]
         self.model["gen"] = Generator(size=size, D=self.cfg["model"]["D"])
+        self.ema_model["gen"] = Generator(size=size, D=self.cfg["model"]["D"])
         self.model["disc"] = Discriminator(K=self.K, size=size)
         self.model["map"] = MappingNetwork(K=self.K, D=self.cfg["model"]["D"])
+        self.ema_model["map"] = MappingNetwork(K=self.K, D=self.cfg["model"]["D"])
         self.model["se"] = StyleEncoder(K=self.K, D=self.cfg["model"]["D"], size=size)
+        self.ema_model["se"] = StyleEncoder(
+            K=self.K, D=self.cfg["model"]["D"], size=size
+        )
+
+        self.ema_model["se"] = copy.deepcopy(self.model["se"])
+        self.ema_model["map"] = copy.deepcopy(self.model["map"])
+        self.ema_model["gen"] = copy.deepcopy(self.model["gen"])
 
         # self.BS = BS
         # self.K = K
@@ -149,6 +160,13 @@ class Trainer:
                 self.optimizer_s.step()
                 self.optimizer_m.step()
 
+                self.moving_average(self.model["se"], self.ema_model["se"], 0.99)
+                self.moving_average(self.model["se"], self.ema_model["se"], 0.99)
+                self.moving_average(self.model["gen"], self.ema_model["gen"], 0.99)
+                # self.soft_copy_param(self.ema_model["se"], self.model["se"], 0.99)
+                # self.soft_copy_param(self.ema_model["map"], self.model["map"], 0.99)
+                # self.soft_copy_param(self.ema_model["gen"], self.model["gen"], 0.99)
+
                 if (self.log) and (step % self.cfg["training"]["log_steps"] == 0):
                     gnorm_g = self.get_grad_norm(self.model["gen"])
                     gnorm_d = self.get_grad_norm(self.model["disc"])
@@ -218,3 +236,36 @@ class Trainer:
             norm_type,
         )
         return total_norm.item()
+
+    def soft_copy_param(target_link, source_link, tau):
+        target_params = dict(target_link.namedparams())
+        for param_name, param in source_link.namedparams():
+            target_params[param_name].data[:] *= 1 - tau
+            target_params[param_name].data[:] += tau * param.data
+
+        # Soft-copy Batch Normalization's statistics
+        target_links = dict(target_link.namedlinks())
+        for link_name, link in source_link.namedlinks():
+            if isinstance(link, L.BatchNormalization):
+                target_bn = target_links[link_name]
+                target_bn.avg_mean[:] *= 1 - tau
+                target_bn.avg_mean[:] += tau * link.avg_mean
+                target_bn.avg_var[:] *= 1 - tau
+                target_bn.avg_var[:] += tau * link.avg_var
+
+    def copy_param(self, target_link, source_link):
+        target_params = dict(target_link.namedparams())
+        for param_name, param in source_link.namedparams():
+            target_params[param_name].data[:] = param.data
+
+        # Copy Batch Normalization's statistics
+        target_links = dict(target_link.namedlinks())
+        for link_name, link in source_link.namedlinks():
+            if isinstance(link, L.BatchNormalization):
+                target_bn = target_links[link_name]
+                target_bn.avg_mean[:] = link.avg_mean
+                target_bn.avg_var[:] = link.avg_var
+
+    def moving_average(self, model, model_test, beta=0.999):
+        for param, param_test in zip(model.parameters(), model_test.parameters()):
+            param_test.data = torch.lerp(param.data, param_test.data, beta)
